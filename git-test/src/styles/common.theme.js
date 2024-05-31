c8a28f1d64dd2877528ecf1fcd130dbf8b3c5649
mariof16 <dinomario@outlook.es>
2024-05-31 09:30:04 +0200

    crear common.theme

---
+++ git-test/src/styles/common.theme.js
@@ -0,0 +1,41 @@
+const colors = {
+    primary: {
+      main: "#0F4561",
+      dark: "#073045",
+    },
+    text: {
+      primary: "#000000",
+    },
+    secondary: {
+      main: "#2C9ED6",
+      dark: "#2781AD",
+    },
+    warning: "#ed6c02",
+    background: {
+      default: "#F3F5F6",
+    },
+    white: "#FFFFFF",
+    black: "#0B0B0B",
+    grey: {
+      100: "#898989",
+      200: "#4E4E4E",
+      300: "#E1E1E1",
+      400: "#E0E0E0",
+      500: "#BDBDBD",
+    },
+    chip: {
+      neoKaizen: "#E0B5C0",
+      neoIkigai: "#B4EEEF",
+      neoTalentUP: "#F6D2B0",
+      neoSkills: "#B8D7FB",
+      neoPRO: "#CBC9FF",
+      neoAcademy: "#CDF3C7",
+      "Social Learning": "#FFEAA1",
+    },
+    form: {
+      background: "#F5FAFD",
+    },
+    yellow: "#DB9121",
+  };
+  
+  export default colors;
