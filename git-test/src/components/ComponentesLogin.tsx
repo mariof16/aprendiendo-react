eedc559d97275ea56694ee1bad6a007ee7a5d050
mariof16 <dinomario@outlook.es>
2024-06-03 12:19:13 +0200

    Ajustar tamaños a porcentual

--- git-test/src/components/ComponentesLogin.tsx
+++ git-test/src/components/ComponentesLogin.tsx
@@ -1,17 +1,173 @@
-import { Card} from "@mui/material"
+import { Card, Box,Typography,TextField,InputAdornment,IconButton,Button} from "@mui/material"
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import colors from "../styles/colors.theme"
 import React from "react";
 
 export const CardLogin = ({ children }: { children: React.ReactNode }) => {
     return (
         <Card sx={{
-            width: "552px",
-            height: "600px",
+            width: "35%",
             margin: "auto",
-            padding: "45px",
+            padding: "2rem",
             backgroundColor: colors.background.default,
         }}>
              {children}
         </Card>
     );
 };
+
+export const LogoLogin = () => (
+    <Box
+        component="img"
+        src="../../public/logologin.png"
+        alt="User Photo"
+        sx={{
+          width: "20%",
+          height: "20%",
+          mb: "2vw",
+          display: 'block',
+          margin: 'auto',
+        }}
+    />
+);
+
+export const TypographyBienvenido = () => (
+    <Typography
+        variant="h4"
+        style={{
+            fontFamily: 'Lato',
+            fontSize: '2.8vw',
+            fontWeight: 400,
+            lineHeight: '4.8vw',
+            letterSpacing: '0.14000000059604645vw',
+            textAlign: 'center',
+            color: colors.text.primary,
+        }}
+    >
+        Bienvenid@ a
+    </Typography>
+)
+
+export const TypographyNeoTools = () => (
+    <Typography
+              variant="h4"
+              style={{
+                fontFamily: 'Lato',
+                fontSize: '2.8vw',
+                fontWeight: 700,
+                lineHeight: '1vw',
+                letterSpacing: '0.14000000059604645px',
+                textAlign: 'center',
+                color: colors.yellow,
+              }}
+            >
+              neoTOOLS{' '}
+              <BoxByNeock />
+    </Typography>
+)
+
+const BoxByNeock = () => (
+    <Box
+      component="span"
+      style={{
+        fontFamily: 'Lato',
+        fontSize: '2vw',
+        fontWeight: 700,
+        lineHeight: '39%',
+        letterSpacing: '0.14000000059604645%',
+        textAlign: 'center',
+        color: colors.grey[500],
+      }}
+    >
+      by neoCK
+    </Box>
+  )
+export const InputMail = () => (
+    <TextField
+      sx={{
+        mt:"2vh",
+        mb: "5vh",
+        width: "34.3vw",
+        height: "8vh", 
+        "& .MuiInputBase-input": {
+          fontSize: "1.5vw"
+        },
+        "& .MuiInputLabel-root": {
+          fontFamily: "Arial", // Cambia la fuente del label
+          fontSize: "1.2vw" // Cambia el tamaño de la fuente del label
+        }
+      }}
+      variant="outlined"
+      label="Email"
+      placeholder="email@ejemplo.es"
+      InputLabelProps={{ shrink: true }}
+    />
+  );
+  
+  export const InputPassword = () => (
+    <TextField
+      type="password"
+      variant="outlined"
+      size="medium"
+      fullWidth
+      label="Contraseña"
+      placeholder="************"
+      InputLabelProps={{ shrink: true }}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton >
+              <VisibilityIcon sx={{ width: "1.5vw", height: "1.5vw" }}/>
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+      sx={{
+        mb: "5vh",
+        width: "34.3vw",
+        height: "8vh",
+        "& .MuiInputBase-input": {
+          fontSize: "1.5vw"
+        },
+        "& .MuiInputLabel-root": {
+          fontFamily: "Arial",
+          fontSize: "1.2vw"
+        }
+      }}
+    />
+  );  
+
+export const ForgotPassword = () => (
+    <Typography
+        variant="h4"
+        sx={{
+        fontFamily: 'Lato',
+        fontSize: '1.3vw', // Escala el tamaño de la fuente
+        fontWeight: 400,
+        lineHeight: '2vw', // Escala la altura de línea
+        letterSpacing: '0.14000000059604645px',
+        textAlign: 'center',
+        textDecoration: "underline",
+        marginTop: "3vh", // Escala el margen superior
+        color: colors.primary.main,
+        }}
+    >
+        ¿Has olvidado tu contraseña?
+  </Typography>
+)
+
+export const LoginButton = () => (
+    <Button
+      size="large"
+      color="primary"
+      variant="contained"
+      sx={{
+        width: "15vw", // Ajusta el ancho del botón
+        height: "8vh", // Ajusta la altura del botón
+        fontSize: "1vw", // Ajusta el tamaño de la fuente del botón
+        marginTop: "3vh" // Ajusta el margen superior del botón
+      }}
+    >
+      INICIAR SESIÓN
+    </Button>
+  );
--- git-test/src/components/Login.tsx
+++ git-test/src/components/Login.tsx
@@ -1,8 +1,6 @@
-import { Box, Typography, TextField, InputAdornment, IconButton, Button } from '@mui/material';
+import { Box } from '@mui/material';
 import { NavButton } from '../routes/NavButton';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import colors from '../styles/colors.theme';
-import { CardLogin } from './ComponentesLogin';
+import { CardLogin,InputMail,LogoLogin, TypographyBienvenido ,TypographyNeoTools,InputPassword, ForgotPassword, LoginButton} from './ComponentesLogin';
 
 export function Login() {
   return (
@@ -10,123 +8,21 @@ export function Login() {
       <NavButton text="Home" href="/home" />
       <CardLogin>
         <Box sx={{ padding: "7px" }}>
-          <Box
-            component="img"
-            src="../../public/logologin.png"
-            alt="User Photo"
-            sx={{
-              width: 81,
-              height: 80,
-              mb: 2,
-              display: 'block',
-              margin: 'auto',
-            }}
-          />
+          <LogoLogin />
           <Box sx={{ marginTop: '20px' }}>
-            <Typography
-              variant="h4"
-              style={{
-                fontFamily: 'Lato',
-                fontSize: '32px',
-                fontWeight: 400,
-                lineHeight: '48px',
-                letterSpacing: '0.14000000059604645px',
-                textAlign: 'center',
-                color: colors.text.primary,
-              }}
-            >
-              Bienvenid@ a
-            </Typography>
-            <Typography
-              variant="h4"
-              style={{
-                fontFamily: 'Lato',
-                fontSize: '32px',
-                fontWeight: 700,
-                lineHeight: '48px',
-                letterSpacing: '0.14000000059604645px',
-                textAlign: 'center',
-                color: colors.yellow,
-              }}
-            >
-              neoTOOLS{' '}
-              <Box
-                component="span"
-                style={{
-                  fontFamily: 'Lato',
-                  fontSize: '26px',
-                  fontWeight: 700,
-                  lineHeight: '39px',
-                  letterSpacing: '0.14000000059604645px',
-                  textAlign: 'center',
-                  color: colors.grey[500],
-                }}
-              >
-                by neoCK
-              </Box>
-            </Typography>
+            <TypographyBienvenido/>
+            <TypographyNeoTools/>
           </Box>
         </Box>
         <Box sx={{marginTop:"20px"}}>
-          <TextField
-            variant="outlined"
-            size="medium"
-            fullWidth
-            label="Email"
-            placeholder="email@ejemplo.es"
-            InputLabelProps={{ shrink: true }}
-          />
-          <TextField
-            variant="outlined"
-            size="medium"
-            fullWidth
-            label="Contraseña"
-            placeholder="************"
-            InputLabelProps={{ shrink: true }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    edge="end"
-                  >
-                    <VisibilityIcon />
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
+          <InputMail/>
+          <InputPassword/>
         </Box>
-        <Typography
-              variant="h4"
-              style={{
-                fontFamily: 'Lato',
-                fontSize: '16px',
-                fontWeight: 400,
-                lineHeight: '16px',
-                letterSpacing: '0.14000000059604645px',
-                textAlign: 'center',
-                textDecoration:"underline",
-                marginTop:"30px",
-                color: colors.primary.main,
-              }}
-            >
-              ¿Has olvidado tu contraseña?
-        </Typography>
+        <ForgotPassword/>
         <Box sx={{ textAlign: 'center', marginTop: '50px' }}>
-          <Button
-            size="large"
-            color="primary"
-            disabled={false}
-            variant="contained"
-            startIcon={null}
-            endIcon={null}
-          >
-            INICIAR SESIÓN
-          </Button>
+          <LoginButton/>
         </Box>
       </CardLogin>
     </>
   );
 }
-
